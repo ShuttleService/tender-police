457fb4c3c6a0e522789ea6d4630bb2e9d9ef7599
Zorodzayi Mukuya <mukuya@gmail.com>
2016-04-21 19:28:10 +0200

    Initial Commit

---
+++ src/main/java/org/dough/apps/tenderpolice/App.java
@@ -0,0 +1,13 @@
+package org.dough.apps.tenderpolice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
